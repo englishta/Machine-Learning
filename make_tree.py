53b1218e09ed00891560308fe0cff5ed479066de
oliver <1999ju373@gmail.com>
2021-10-27 08:43:40 +0900

    update

--- make_tree.py
+++ make_tree.py
@@ -1,56 +1,79 @@
-node = []
-with open('out.txt', mode='r') as f:
-    for line in f.readlines():
-        line = line.replace('\n', '')
-        node.append(line)
+def make_tree():
+    node = []
+    with open('out.txt', mode='r') as f:
+        for line in f.readlines():
+            line = line.replace('\n', '')
+            node.append(line)
 
-n = len(node)
-depth = [-1]*n # depth[i]:=is–Ú‚Ìƒm[ƒh‚Ì[‚³
+    n = len(node)
+    depth = [-1]*n # depth[i]:=i?????????
 
-for i in range(n):
-    depth[i] = (len(node[i])-len(node[i].lstrip(' ')))//2
-print(depth)
+    for i in range(n):
+        depth[i] = (len(node[i])-len(node[i].lstrip(' ')))//2
+    
+    for i in range(n):
+        node[i] = node[i].lstrip(' ')
 
-# for i in range(n): node[i] = node[i].lstrip(' ')
+    r = [-1]*n
+    r[0] = 0
+    graph = [[] for _ in range(n)]
 
-r = [-1]*n
-r[0] = 0
-graph = [[] for _ in range(n)]
+    for i in range(1, n):
+        r[depth[i]] = i
+        graph[r[depth[i]-1]].append(i)
+        graph[i].append(r[depth[i]-1])
+    
+    return node, graph
 
-for i in range(1, n):
-    r[depth[i]] = i
-    graph[r[depth[i]-1]].append(i)
-    graph[i].append(r[depth[i]-1])
-
-print(graph)
-
-# for e in graph[2]:
-    # print(node[e])
-
-# [‚³—Dæ’Tõ
-def dfs(now):
-    print(node[now])
+def dfs_struct_search(now, graph, node, struct_list):
+    if now >=20: return
+    if 'Struct:' in node[now]:
+        struct_list.append(node[now].replace('Struct:', '').replace(' ', ''))
     for next in graph[now]:
         if next < now: continue
-        dfs(next)
+        dfs_struct_search(next, graph, node, struct_list)
 
-dfs(0)
-# •¡ŽG‚È[‚³—Dæ’Tõ
-# struct_list = [] #\‘¢‘Ì–¼‚ð‹L˜^‚·‚éƒŠƒXƒg
-# array_list = []
-# main_on = False
+def dfs_array_search1(now, graph, node, array_root):
+    if 'ArrayDecl:' in node[now]:
+        array_root.append(now)
+    for next in graph[now]:
+        if next < now: continue
+        dfs_array_search1(next, graph, node, array_root)
 
-# def DFS(now):
-#     for next in graph[now]:
-#         if next < now: continue
-#         if 'Decl:main' in node[i]:
-#             main_on = True
-#         if not main_on:
-#             # mainŠÖ”ŠO‚Ì“Ç‚ÝŽæ‚è
-#             if 'Struct:' in node[now]:
-#                 struct_list.append(node[now].replace('Struct', ''))
-#         else:
-#             # mainŠÖ”“à‚Ì“Ç‚ÝŽæ‚è
-#             if 'ArrayDecl:' in node[i]:
-#                 array_list.append(now)
-#         dfs(next)
+def dfs_array_search2(now, graph, node, name, type, size):
+    if 'ID:' in node[now]:
+        size = node[now].replace('ID:', '').replace(' ', '')
+    for next in graph[now]:
+        if next < now: continue
+        name, type, size = dfs_array_search2(next, graph, node, name, type, size)
+    return name, type, size
+
+# ?????????????        
+class Array():
+    def __init__(self, name, type, size):
+        self.name = name
+        self.type = type
+        self.size = size
+
+def main(): 
+    node, graph = make_tree()
+    array_root = [] # ???????????
+    array_list = [] # Array???????????
+    struct_list = []
+    dfs_struct_search(0, graph, node, struct_list)
+    dfs_array_search1(20, graph, node, array_root)
+    for id in array_root:
+        name, type, size = dfs_array_search2(id, graph, node, "", "", "")
+        comp = Array(name, type, size)
+        array_list.append(comp)
+    
+    print(array_root)
+    print(struct_list)
+    for e in array_list:
+        print(e.size)
+        print(e.type)
+        print(e.name)
+    
+
+if __name__ == "__main__":
+    main()
